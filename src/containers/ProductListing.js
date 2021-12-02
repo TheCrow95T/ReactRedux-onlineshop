d61046c3af601269fbe8cbeb89bd3bdd4eb5dc1a
TheCrow95T <calvinongjiajun@gmail.com>
2021-12-02 18:28:25 +0800

    last

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,9 +1,24 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, {useEffect} from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setProducts } from '../redux/actions/productAction'
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
     const products = useSelector((state) => state)
+    const dispatch = useDispatch();
+
+    const fetchProducts = async ()=> {
+        const response = await axios.get ("http://fakestoreapi.com/products")
+                                    .catch((err)=>{
+                                        console.log("Error",err);
+                                    });
+        dispatch(setProducts(response.data));
+    }
+
+    useEffect(()=>{
+        fetchProducts()
+    },[])
 
     return (
         <div className="ui grid container">
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,23 @@
 import { ActionTypes } from "../contants/actions-type";
 
 const initialState = {
-    product:[
-        {
-        id: 1,
-        title: "I'm a title",
-        category: "string", 
-        },
-    ],
+    products: [],
 };
 export const productReducer =(state=initialState,{type,payload}) => {
     switch(type){
         case ActionTypes.SET_PRODUCTS:
+            return {...state, products: payload};
+        default:
             return state;
+    }
+};
+
+export const selectProductReducer =(state={},{type,payload}) => {
+    switch(type){
+        case ActionTypes.SELECTED_PRODUCT:
+            return {...state, ...payload};
+        case ActionTypes.REMOVE_SELECTED_PRODUCT:
+            return {};
         default:
             return state;
     }
